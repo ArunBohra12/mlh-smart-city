ccd8e67b1f111853de5f2c944d4ff43e32cff7d3
Garvit1809 <garvitvarshne18@gmail.com>
2023-04-23 15:19:49 +0530

    Styling Single Page

--- client/src/utils/globalConstants.js
+++ client/src/utils/globalConstants.js
@@ -1,1 +1,14 @@
 export const localStorageUser = "mlh-smart-city-user";
+
+const months = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
+
+export const getReadableTime = (ISODate) => {
+  const readable = new Date(ISODate);
+  const month = readable.getMonth();
+  const date = readable.getDate();
+  const year = readable.getFullYear();
+
+  const monthLong = months[month];
+  const fulldate = monthLong + " " + date + ", " + year;
+  return fulldate;
+};
