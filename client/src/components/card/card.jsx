f1e3062b91d322208cebde5fd9ab995aa6d2d1ce
Garvit1809 <garvitvarshne18@gmail.com>
2023-04-23 12:02:21 +0530

    mapping issue data

--- client/src/components/card/card.jsx
+++ client/src/components/card/card.jsx
@@ -1,22 +1,109 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./card.css";
 import { FaMapMarkerAlt } from "react-icons/fa";
-function Card() {
-  return (
-    <div className='card'>
-      <img
-        className='card-image'
-        src='https://www.deccanherald.com/sites/dh/files/articleimages/2022/08/22/pothole-1138292-1661174905.jpg'
-        alt=''
-      />
-      <h3 className='location-text'>
-        <FaMapMarkerAlt /> ramghat,aligarh
-      </h3>
-      <div className='card-text'>
-        <h3>Description</h3>
-        <h4>lorem ipsum lorem lorem ipsum lorem lorem ipsum lorem lorem ipsum lorem ipsum</h4>
-        <h3>Raised by: Ash</h3>
+import { API, BASE_URL } from "../../utils/APIRoutes";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+function Card(props) {
+  const issue = props.issue;
+
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    async function fetchUserData() {
+      const data = await JSON.parse(localStorage.getItem(localStorageUser));
+      setUserData(data);
+    }
+    fetchUserData();
+  }, []);
+
+  const approveIssueHandler = async (e) => {
+    const { data } = await axios.patch(
+      `${BASE_URL}${API}/issue/${issue._id}/approve`,
+      {},
+      {
+        headers: getHeaders(userData.token),
+      }
+    );
+    console.log(data);
+    if (data.status === "success") {
+      window.location.reload();
+    }
+  };
+
+  const closeissueHandler = async (e) => {
+    // e.preventDefauslt();
+    const { data } = await axios.patch(
+      `${BASE_URL}${API}/issue/${issue._id}/close`,
+      {},
+      {
+        headers: getHeaders(userData.token),
+      }
+    );
+    console.log(data);
+    if (data.status === "success") {
+      window.location.reload();
+    }
+  };
+
+  const navigate = useNavigate();
+  const singlePageNavigator = () => {
+    navigate(`/issues/${issue._id}`);
+  };
+
+  return issue === undefined ? null : (
+    <div className='card' onClick={singlePageNavigator} >
+      <div className='issue-content'>
+        <p>{issue.issueContent}</p>
+        <div className='issue-img'>
+          <img src={issue.issuePics} alt='' />
+        </div>
       </div>
+      <div className='location'>
+        <FaMapMarkerAlt />
+        <h4>
+          {issue.locationAddressFirstLine} {issue.locationAddressSecondLine}, {issue.locationCity}, {issue.postalCode}
+        </h4>
+      </div>
+      <div className='author-details'>
+        <h5>Raise By :-</h5>
+        <div>
+          <div className='img-container'>
+            <img src={issue.issueRaisedBy.photo} alt='' />
+          </div>
+          <h3>{issue.issueRaisedBy.name}</h3>
+        </div>
+      </div>
+
+      {issue.isIssueApproved ? (
+        <div className='approved-by'>
+          <h5>Approved By :-</h5>
+          <div>
+            <div className='image-container'>
+              <img src={issue.issueApprovedBy.photo} alt='' />
+            </div>
+            <h3>{issue.issueApprovedBy.name}</h3>
+          </div>
+        </div>
+      ) : null}
+      {issue.isIssueResolved ? (
+        <div className='closedBy'>
+          <h5>Closed By :-</h5>
+          <div>
+            <div className='image-container'>
+              <img src={issue.issueClosedBy.photo} alt='' />
+            </div>
+            <h3>{issue.issueClosedBy.name}</h3>
+          </div>
+        </div>
+      ) : null}
+      {!issue.isIssueApproved && userData.isHelper ? (
+        <button onClick={approveIssueHandler}>Approve Issue</button>
+      ) : null}
+      {issue.isIssueApproved && !issue.isIssueResolved && userData.isHelper && issue.issueUpdates.length > 0 ? (
+        <button onClick={closeissueHandler}>Close Issue</button>
+      ) : null}
     </div>
   );
 }
--- client/src/pages/issues/issues.jsx
+++ client/src/pages/issues/issues.jsx
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../../components/card/card";
 import "./issues.css";
 import Navbar from "../../components/navbar/navbar";
 import PostIssue from "../../components/postIssue/postIssue";
+import axios from 'axios'
+import { API, BASE_URL } from "../../utils/APIRoutes";
 
 function Issues() {
+  const [issues, setIssues] = useState([]);
+
+  async function fetchUnApprovedIssues() {
+    const { data } = await axios.get(`${BASE_URL}${API}/issue/unapproved`);
+    console.log(data);
+    setIssues(data.unapprovedIssues);
+  }
+
+  useEffect(() => {
+    fetchUnApprovedIssues();
+  }, []);
+
   return (
     <>
       <Navbar />
-      <PostIssue/>
+      <PostIssue />
       <div className='issues'>
-        <Card />
-        <Card />
+        {
+          issues && issues.map((issue,index) => {
+            return <Card key={index} issue={issue} />
+          })
+        }
         <Card />
       </div>
     </>
