dff1ed3372db0b532c585c85b4c020966854b83d
Arun Bohra <arunbohra12@gmail.com>
2023-04-23 12:55:06 +0530

    Fix merge conflicts

--- client/src/pages/issues/issues.jsx
+++ client/src/pages/issues/issues.jsx
@@ -1,12 +1,9 @@
 import React, { useEffect, useState } from "react";
 import Card from "../../components/card/card";
 import PostIssue from "../../components/postIssue/postIssue";
-<<<<<<< HEAD
 import "./issues.css";
-=======
-import axios from 'axios'
+import axios from "axios";
 import { API, BASE_URL } from "../../utils/APIRoutes";
->>>>>>> f8ffd0140827da9c668742e89ab29dcac581645a
 
 function Issues() {
   const [issues, setIssues] = useState([]);
@@ -23,17 +20,12 @@ function Issues() {
 
   return (
     <>
-<<<<<<< HEAD
-=======
-      <Navbar />
->>>>>>> f8ffd0140827da9c668742e89ab29dcac581645a
       <PostIssue />
       <div className='issues'>
-        {
-          issues && issues.map((issue,index) => {
-            return <Card key={index} issue={issue} />
-          })
-        }
+        {issues &&
+          issues.map((issue, index) => {
+            return <Card key={index} issue={issue} />;
+          })}
         <Card />
       </div>
     </>
